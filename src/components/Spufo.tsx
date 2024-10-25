0e2b3375bf964666fedce42a1249ffa40a06bd63
Jordan Suter <16938311+JOrbits@users.noreply.github.com>
2024-10-26 00:06:16 +0200

    spufo

---
+++ src/components/Spufo.tsx
@@ -0,0 +1,21 @@
+import Hero from './Hero';
+import About from './About';
+import Features from './Features';
+import SmartphoneAdvantages from './SmartphoneAdvantages';
+import Testimonial from './Testimonial';
+import Contact from './Contact';
+
+const Spufo = () => {
+  return (
+    <>
+        <Hero />
+        <About />
+        <Features />
+        <SmartphoneAdvantages />
+        <Testimonial />
+        <Contact />
+    </>
+  );
+};
+
+export default Spufo;
