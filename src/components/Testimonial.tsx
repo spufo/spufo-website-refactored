1a59a0e2099da052a456dcb5d3fc44fd21e89d5d
Jordan Suter <16938311+JOrbits@users.noreply.github.com>
2024-10-25 21:23:08 +0200

    testimonial pop

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -10,13 +10,13 @@ const Testimonial = () => {
           <Quote className="absolute top-0 left-0 w-16 h-16 text-[#FF6600] opacity-25 transform -translate-x-1/2 -translate-y-1/2" />
           <blockquote className="text-2xl md:text-4xl font-light italic text-center mb-8 relative z-10">
             «Das Beispiel 
-            <span className="text-[#FF6600] font-normal hover:scale-[1.05] transition-all duration-300 inline-block mx-2">SPUFO</span>
+            <span className="text-[#FF6600] font-normal hover:scale-[1.02] transition-all duration-300 inline-block mx-2">SPUFO</span>
              zeigt auf, wie bedeutsam neue Technologien
              auch für die Kriminaltechnik sind.» ...<br/> 
              «Die App sorgt für deutlich  
-             <span className="text-[#FF6600] font-normal hover:scale-[1.05] transition-all duration-300 inline-block mx-2">mehr Effizienz</span> 
+             <span className="text-[#FF6600] font-normal hover:scale-[1.02] transition-all duration-300 inline-block mx-2">mehr Effizienz</span> 
              und vor allem  
-             <span className="text-[#FF6600] font-normal hover:scale-[1.05] transition-all duration-300 inline-block mx-2">mehr eingelieferte Spuren</span>.»
+             <span className="text-[#FF6600] font-normal hover:scale-[1.02] transition-all duration-300 inline-block mx-2">mehr eingelieferte Spuren</span>.»
           </blockquote>
           <div className="text-center">
             <p className="text-xl font-semibold mb-1">Dominik Hänni</p>
