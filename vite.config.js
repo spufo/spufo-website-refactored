202af929b4b5bb8fc6227c8a3cec65bb6c825286
Jordan Suter <16938311+JOrbits@users.noreply.github.com>
2024-10-26 16:00:54 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/spufo-website-refactored/',  // Replace 'your-repo-name' with the actual name of your GitHub repository
 });
