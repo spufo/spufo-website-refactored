60030dba2f284983d85a39daa6319abcd22cebf1
Jordan Suter <16938311+JOrbits@users.noreply.github.com>
2024-11-19 14:50:15 -0800

    change base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/spufo-website-refactored/',  // Replace 'your-repo-name' with the actual name of your GitHub repository
+  base: '/',  // Replace 'your-repo-name' with the actual name of your GitHub repository
 });
