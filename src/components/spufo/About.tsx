9f565772518716537ca922a472dec20b46e8cb12
Jordan Suter <16938311+JOrbits@users.noreply.github.com>
2024-11-19 22:36:48 -0800

    fix about hover

--- src/components/spufo/About.tsx
+++ src/components/spufo/About.tsx
@@ -11,19 +11,17 @@ const About = () => {
     <section id="about" className="relative py-20 px-6 bg-gradient-to-br from-[#1E1E2E] to-[#2A2A3A] overflow-hidden" >
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between">
         {/* Fingerprint background */}
-        <div 
+        <div
           className="absolute invert translate-x-80 rotate-[-134deg] sm:rotate-[-134deg] scale-[2] sm:scale-[0.8] bg-contain bg-no-repeat bg-center opacity-100 z-0 pointer-events-none"
         >
           <img src={background} alt="Background Pattern" />
         </div>
-        
+
         <div className="md:w-1/2 mb-10 md:mb-0 flex flex-col items-center">
-          <motion.img 
+          <motion.img
             src={aboutImage}
-            alt="SPUFO in action" 
+            alt="SPUFO in action"
             className="rounded-3xl shadow-2xl w-2/3 h-auto transform hover:scale-105 transition duration-300 z-0"
-            initial={{ opacity: 0, scale: 0.9 }}
-            whileInView={{ opacity: 1, scale: 1 }}
             viewport={{ once: true }}
             transition={{ duration: 0.3 }}
           />
@@ -40,7 +38,7 @@ const About = () => {
             <h2 className="text-4xl md:text-7xl font-bold text-white mb-6">
               {t('aboutTitle')}
             </h2>
-            
+
             <p className="text-white text-lg mb-6">
               {t('aboutDescription').split('SPUFO').map((part, index, array) => (
                 <React.Fragment key={index}>
